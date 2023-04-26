213f651d432e6aeb19f3af441ec6ea3f3b079ef0
cs20b041 <cs20b041@iittp.ac.in>
2023-04-26 23:51:21 +0530

    Some changes

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,53 @@
-const app = require("./app");
-const connectDatabase = require("./db/Database");
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
 
-// Handling uncaught Exception
-process.on("uncaughtException", (err) => {
-	console.log(`Error: ${err.message}`);
-	console.log(`shutting down the server for handling uncaught exception`);
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+app.get("/api/keys/google", (req, res) => {
+  res.send({ key: process.env.GOOGLE_API_KEY || "" });
 });
 
-// config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-	require("dotenv").config({
-		path: "backend/config/.env",
-	});
-}
+app.use("/api/upload", uploadRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
-// connect db
-connectDatabase();
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
-// create server
-const server = app.listen(process.env.PORT, () => {
-	console.log(`Server is running on http://localhost:${process.env.PORT}`);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
-// unhandled promise rejection
-process.on("unhandledRejection", (err) => {
-	console.log(`Shutting down the server for ${err.message}`);
-	console.log(`shutting down the server for unhandle promise rejection`);
-
-	server.close(() => {
-		process.exit(1);
-	});
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
 });
